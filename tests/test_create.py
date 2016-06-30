aa417fab34296281dcbf43dca2814197e60a0a1e
Casey Law <caseyjlaw@gmail.com>
2016-06-30 15:47:17 -0700

    adding test for successful initial ag repo

--- tests/test_create.py
+++ tests/test_create.py
@@ -1,6 +1,10 @@
 import activegit
 import pytest
 
-def test_creation():
+def test_nocreation():
     with pytest.raises(TypeError):
         activegit.ActiveGit()
+
+def test_creation():
+    ag = activegit.ActiveGit('testrepo')
+    assert ag.version == 'initial'
