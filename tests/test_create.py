1598068228e2a9826884296fcc5f0d683541a3c4
Casey Law <caseyjlaw@gmail.com>
2016-07-07 14:01:56 -0700

    adding more create tests

--- tests/test_create.py
+++ tests/test_create.py
@@ -1,10 +1,24 @@
 import pytest
 import activegit
 
-def test_nocreation():
+def test_create0():
     with pytest.raises(TypeError):
         activegit.ActiveGit()
 
-def test_creation():
+def test_create1():
     ag = activegit.ActiveGit('testrepo')
     assert ag.version == 'initial'
+
+def test_create2():
+    ag = activegit.ActiveGit('testrepo')
+    assert ag.versions == ['initial']
+
+def test_create3():
+    ag = activegit.ActiveGit('testrepo')
+    assert ag.isvalid == True
+
+def test_create4():
+    ag = activegit.ActiveGit('testrepo')
+    ag = activegit.ActiveGit('testrepo')
+    assert ag.isvalid == True
+
